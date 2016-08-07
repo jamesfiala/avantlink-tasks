d81f744fdf72c910ed5c555ff9f2811391a3505d
James Fiala <jrfiala@xactware.com>
2016-08-06 23:21:10 -0600

    Updated services index.ts

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,6 +1,6 @@
 import { TasksService } from './tasks-service';
 
-export { TasksService };
+export { TasksService } from './tasks-service';
 
 export const TASKS_SERVICE_PROVIDERS: any[] = [
 	TasksService
